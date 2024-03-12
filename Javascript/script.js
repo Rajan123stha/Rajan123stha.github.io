68e6e40cbec5dd77ae5560dcdc5f7a947ff280f5
Rajan <rajan1234stha@gmail.com>
2024-03-12 22:40:45 +0545

    added project info

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,50 +1,70 @@
 /*menu icon navbar */
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 };
+
+//For CV download
+const downloadCV = document.getElementById("download-cv");
+downloadCV.addEventListener("click", function () {
+  var CVPath = "../image/CV.png";
+  var downloadLink = document.createElement("a");
+  downloadLink.href = CVPath;
+  downloadLink.download = "CV.png";
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+  console.log("clicked");
+});
+
 /*scroll section active link*/
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-        if (top >= offset && top < offset + height) {
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        }
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 
-    });
-
-    /* sticky navbar */
-    let header = document.querySelector('.header');
-    header.classList.toggle('sticky', window.scrollY > 100);
-    /*remove menu icon navbar when click navbar link(scroll)*/
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
+  /* sticky navbar */
+  let header = document.querySelector(".header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+  /*remove menu icon navbar when click navbar link(scroll)*/
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 /* Dark Mode */
-let darkModeIcon = document.querySelector('#darkMode');
-darkModeIcon.onclick=()=> {
-    darkModeIcon.classList.toggle('bx-sun');
-    document.body.classList.toggle('dark-mode');
+let darkModeIcon = document.querySelector("#darkMode");
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle("bx-sun");
+  document.body.classList.toggle("dark-mode");
 };
 /* scroll reveal */
 ScrollReveal({
-    reset: true,
-    distance:'80px',
-    duration:2000,
-    delay:200
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
 });
-ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
-ScrollReveal().reveal('.home-img img, .services-container, .project-box, .contact form', { origin:'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img img', { origin:'left' });
-ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin:'right' });
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img img, .services-container, .project-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img img", { origin: "left" });
+ScrollReveal().reveal(".home-content h3, .home-content p, .about-content", {
+  origin: "right",
+});
